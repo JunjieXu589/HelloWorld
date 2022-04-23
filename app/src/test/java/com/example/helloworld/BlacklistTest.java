a648aec21f3dfda7f800a5b580d5c3ceb147df34
JunjieXu589 <892789948@qq.com>
2022-04-24 00:44:23 +0800

    增加固定头像，首页加了card

---
+++ app/src/test/java/com/example/helloworld/BlacklistTest.java
@@ -0,0 +1,24 @@
+package com.example.helloworld;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BlacklistTest {
+
+    @Test
+    public void getCustomer() {
+    }
+
+    @Test
+    public void setCustomer() {
+    }
+
+    @Test
+    public void getPark() {
+    }
+
+    @Test
+    public void setPark() {
+    }
+}

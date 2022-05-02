a41ec9b0e5f059f8ae81df4d79a0f88e6a29373e
JunjieXu589 <892789948@qq.com>
2022-05-02 18:23:01 +0800

    button改颜色 淡青色

--- app/src/main/java/com/example/helloworld/AddRecord.java
+++ app/src/main/java/com/example/helloworld/AddRecord.java
@@ -28,39 +28,40 @@ public class AddRecord extends AppCompatActivity {
         setContentView(root);//get the binding class
 //        setContentView(R.layout.add_record);//The effect should be consistent with the code above
 
-//        Spinner parkNameSpinner = binding.addRecordParkName;
-//        Button more = binding.addMorePark;//获取页面组件
-//        TextView tiaozhuan = binding.addParkNameTitle;//FOR DEBUG
-        //CardView cardView = binding.addRecordCardView;
+        Spinner parkNameSpinner = binding.addRecordParkName;
+        Button more = binding.addMorePark;//获取页面组件
         Button confirm = binding.addRecordConfirm;
+
 
         parkNameList = new ArrayList<String>();
         initParkNameList();
 
 
-//        parkNameAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, parkNameList);
-//        //parkNameAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        parkNameSpinner.setAdapter(parkNameAdapter);//设置adapter
-//
-//        parkNameSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-//
-//            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> adapterView) {
-//
-//            }
-//        });
+        parkNameAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, parkNameList);
+        parkNameAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);//设置下拉表的样式
+        parkNameSpinner.setAdapter(parkNameAdapter);//设置adapter
 
-//        cardView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(AddRecord.this, Register.class);
-//                startActivity(intent);
-//            }
-//        });
+        parkNameSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {//一般用于没有选项的情况，这种情况很少发生
+
+            }
+        });
+
+
+
+        more.setOnClickListener(new View.OnClickListener() {//button展示数据库里所有的park，并等用户选择后返回position值
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddRecord.this, AllPark.class);
+                startActivityForResult(intent,1);
+            }
+        });
 
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,40 +71,25 @@ public class AddRecord extends AppCompatActivity {
             }
         });
 
-//        more.setOnClickListener(new View.OnClickListener() {//button展示数据库里所有的park，并等用户选择后返回position值
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(AddRecord.this, Register.class);
-//                startActivityForResult(intent,1);
-//            }
-//        });
-
-//        tiaozhuan.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(AddRecord.this, AllPark.class);
-//                startActivityForResult(intent,1);
-//            }
-//        });
 
 
     }
 
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        switch(requestCode){
-//            case 1:
-//                if(resultCode == RESULT_OK){
-//
-//                   //TODO
-//                    int position = data.getIntExtra("park_position",0);
-//                    Log.e("Test",String.valueOf(position));
-//                }
-//            break;
-//            default:
-//        }
-//    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {//当从allpark返回时会调用这个方法
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode){
+            case 1:
+                if(resultCode == RESULT_OK){
+
+                   //TODO
+                    int position = data.getIntExtra("park_position",0);
+                    Log.e("Test",String.valueOf(position));
+                }
+            break;
+            default:
+        }
+    }
 
 
 

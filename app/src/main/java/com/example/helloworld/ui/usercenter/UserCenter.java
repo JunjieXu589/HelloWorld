dfa19e395a3cdd9ff26f236bfbfb7d4cbdab42e1
JunjieXu589 <892789948@qq.com>
2022-05-02 23:00:03 +0800

    引入Glide 实现头像的高斯模糊

--- app/src/main/java/com/example/helloworld/ui/usercenter/UserCenter.java
+++ app/src/main/java/com/example/helloworld/ui/usercenter/UserCenter.java
@@ -7,14 +7,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.helloworld.*;
 import com.example.helloworld.databinding.UserCenterFragmentBinding;
 import com.example.helloworld.entity.Session;
+import jp.wasabeef.glide.transformations.BlurTransformation;
+
+import static com.bumptech.glide.request.RequestOptions.bitmapTransform;
 
 public class UserCenter extends Fragment {
 
@@ -34,8 +41,21 @@ public class UserCenter extends Fragment {
         binding = UserCenterFragmentBinding.inflate(inflater,container,false);
         View root = binding.getRoot();
 
-        TextView account = binding.userCenterAccount;
+        ImageView profileBackground = binding.headBack;
+        ImageView profileImage = binding.imagesHead;//使用glide设置头像比较方便
+        Glide.with(getActivity())
+                .load(R.drawable.profile_1)
+                .apply(RequestOptions.bitmapTransform(new BlurTransformation(4,25)))
+                .apply(RequestOptions.centerCropTransform())
+                .into(profileBackground);
+        Glide.with(getActivity())
+                .load(R.drawable.profile_car)
+                .apply(RequestOptions.circleCropTransform())
+                .into(profileImage);
 
+
+
+        TextView account = binding.userCenterAccount;
         session =new Session();
         account.setText(session.getUserID());//在此之前千万别忘记初始化session！！！
         account.setOnClickListener(new View.OnClickListener() {//给用户名现实的位置设置监听器
